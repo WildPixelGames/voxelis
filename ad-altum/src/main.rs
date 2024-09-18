0a2d8e27e5be2ddafba0b9eb37786a5f7b89e1e3
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2024-09-18 20:03:34 +0200

    Render single chunk

--- ad-altum/src/main.rs
+++ ad-altum/src/main.rs
@@ -1,22 +1,102 @@
+use bevy::pbr::wireframe::{WireframeConfig, WireframePlugin};
 use bevy::{
     diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
     prelude::*,
     window::PresentMode,
 };
+use bevy::color::palettes::basic::SILVER;
 use bevy_screen_diagnostics::{
     ScreenDiagnosticsPlugin, ScreenEntityDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin,
 };
 
 struct GamePlugin;
 
+#[derive(Component)]
+struct Chunk;
+
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
+        app.add_systems(Update, (rotate, toggle_wireframe));
     }
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera3dBundle::default());
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 1., 3.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
+        ..default()
+    });
+
+    // commands.spawn(EnvironmentMapLight {
+    //     diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+    //     specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+    //     intensity: 2_000.0,
+    // });
+    // 
+    commands.spawn(
+        TextBundle::from_section("Press space to toggle wireframes", TextStyle::default())
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                top: Val::Px(12.0),
+                left: Val::Px(12.0),
+                ..default()
+            }),
+    );
+
+    let mut chunk = voxelis::Chunk::new();
+    chunk.generate_data();
+
+    let mesh = chunk.generate_mesh();
+    let mesh = meshes.add(mesh);
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            intensity: 10_000_000.,
+            range: 100.0,
+            shadow_depth_bias: 0.2,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.0, 16.0, 8.0),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10)),
+        material: materials.add(Color::from(SILVER)),
+        ..default()
+    });
+
+    commands
+        .spawn(PbrBundle {
+            mesh,
+            material: materials.add(Color::WHITE),
+            // transform: Transform::from_xyz(0.0, 0.0, -5.0),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(Chunk)
+        .insert(Name::new("Terrain".to_string()));
+}
+
+fn rotate(mut query: Query<&mut Transform, With<Chunk>>, time: Res<Time>) {
+    for mut transform in &mut query {
+        transform.rotate_y(time.delta_seconds() / 2.);
+    }
+}
+
+fn toggle_wireframe(
+    mut wireframe_config: ResMut<WireframeConfig>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        wireframe_config.global = !wireframe_config.global;
+    }
 }
 
 fn main() {
@@ -31,8 +111,9 @@ fn main() {
                 ..default()
             }),
             GamePlugin,
+            WireframePlugin,
             FrameTimeDiagnosticsPlugin,
-            LogDiagnosticsPlugin::default(),
+            // LogDiagnosticsPlugin::default(),
             ScreenDiagnosticsPlugin::default(),
             ScreenFrameDiagnosticsPlugin,
             ScreenEntityDiagnosticsPlugin,
