39fef992b80cb91b11e61e5fbf9f53a98cc0de43
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2024-09-29 17:01:10 +0200

    Cleanup vtm-viewer/src/main.rs

--- vtm-viewer/src/main.rs
+++ vtm-viewer/src/main.rs
@@ -4,15 +4,12 @@ use std::time::Instant;
 use bevy::color::palettes::basic::SILVER;
 use bevy::color::palettes::css::GRAY;
 use bevy::core_pipeline::bloom::BloomSettings;
-use rayon::prelude::*;
-// use bevy::core_pipeline::experimental::taa::{TemporalAntiAliasBundle, TemporalAntiAliasPlugin};
 use bevy::core_pipeline::tonemapping::Tonemapping;
 use bevy::core_pipeline::Skybox;
 use bevy::pbr::wireframe::{WireframeConfig, WireframePlugin};
 use bevy::pbr::{
-    // CascadeShadowConfigBuilder, ScreenSpaceAmbientOcclusionBundle,
-    // ScreenSpaceAmbientOcclusionQualityLevel, ScreenSpaceAmbientOcclusionSettings,
-    VolumetricFogSettings,
+    ScreenSpaceAmbientOcclusionBundle, ScreenSpaceAmbientOcclusionQualityLevel,
+    ScreenSpaceAmbientOcclusionSettings, VolumetricFogSettings,
 };
 use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, window::PresentMode};
 use bevy_egui::EguiPlugin;
@@ -20,10 +17,9 @@ use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 use bevy_screen_diagnostics::{
     ScreenDiagnosticsPlugin, ScreenEntityDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin,
 };
-use voxelis::io::export::export_model_to_vtm;
+use rayon::prelude::*;
 use voxelis::io::import::import_model_from_vtm;
-use voxelis::voxelizer::Voxelizer;
-use voxelis::{obj_reader, Model};
+use voxelis::Model;
 
 struct GamePlugin;
 
@@ -33,63 +29,51 @@ struct Chunk;
 #[derive(Resource)]
 pub struct ModelResource(pub Model);
 
+#[derive(Eq, PartialEq)]
+pub enum MaterialType {
+    Checkered,
+    Gradient,
+}
+
+#[derive(Resource)]
+pub struct ModelSettings {
+    pub material_type: MaterialType,
+}
+
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup);
-        app.add_systems(Update, (rotate, toggle_wireframe));
+        app.add_systems(Update, toggle_wireframe);
     }
 }
 
-// fn generate_chunk_color(index: usize, total: usize) -> Color {
-//     // Normalize the index to generate distinct hues
-//     let hue = (index as f32 / total as f32) * 360.0;
+fn generate_chunk_color_gradient(index: usize, total: usize) -> Color {
+    // Normalize the index to generate distinct hues
+    let hue = (index as f32 / total as f32) * 360.0;
 
-//     // Convert HSL to RGB
-//     let rgb = hsl_to_rgb(hue, 0.7, 0.5); // Saturation: 0.7, Lightness: 0.5
-//     Color::srgba(rgb.0, rgb.1, rgb.2, 1.0)
-// }
-
-// fn hsl_to_rgb(hue: f32, saturation: f32, lightness: f32) -> (f32, f32, f32) {
-//     let c = (1.0 - (2.0 * lightness - 1.0).abs()) * saturation;
-//     let x = c * (1.0 - ((hue / 60.0) % 2.0 - 1.0).abs());
-//     let m = lightness - c / 2.0;
-
-//     let (r, g, b) = match hue {
-//         0.0..=60.0 => (c, x, 0.0),
-//         60.0..=120.0 => (x, c, 0.0),
-//         120.0..=180.0 => (0.0, c, x),
-//         180.0..=240.0 => (0.0, x, c),
-//         240.0..=300.0 => (x, 0.0, c),
-//         _ => (c, 0.0, x),
-//     };
-
-//     (r + m, g + m, b + m)
-// }
-
-fn generate_base_chunk_color(x: i32, y: i32, z: i32) -> Color {
-    // Use prime numbers to create a unique seed for each coordinate
-    let seed = x as f32 * 31.0 + y as f32 * 37.0 + z as f32 * 41.0;
-
-    // Generate color components using trigonometric functions
-    let r = (seed.sin() * 0.5 + 0.5).fract();
-    let g = (seed.cos() * 0.5 + 0.5).fract();
-    let b = ((seed * 0.1).tan() * 0.5 + 0.5).fract();
-
-    Color::srgba(r, g, b, 1.0)
+    // Convert HSL to RGB
+    let rgb = hsl_to_rgb(hue, 0.7, 0.5); // Saturation: 0.7, Lightness: 0.5
+    Color::srgba(rgb.0, rgb.1, rgb.2, 1.0)
 }
 
-// fn generate_chunk_color(x: i32, y: i32, z: i32) -> Color {
-//     let base_color = generate_base_chunk_color(x, y, z);
-//     // Shift the color based on the chunk's position in the grid
-//     let shift = (x + y + z) as f32 * 0.1;
-//     let r = (base_color.to_srgba().red + shift).fract();
-//     let g = (base_color.to_srgba().green + shift * 1.5).fract();
-//     let b = (base_color.to_srgba().blue + shift * 2.0).fract();
+fn hsl_to_rgb(hue: f32, saturation: f32, lightness: f32) -> (f32, f32, f32) {
+    let c = (1.0 - (2.0 * lightness - 1.0).abs()) * saturation;
+    let x = c * (1.0 - ((hue / 60.0) % 2.0 - 1.0).abs());
+    let m = lightness - c / 2.0;
 
-//     Color::srgba(r, g, b, 1.0)
-// }
+    let (r, g, b) = match hue {
+        0.0..=60.0 => (c, x, 0.0),
+        60.0..=120.0 => (x, c, 0.0),
+        120.0..=180.0 => (0.0, c, x),
+        180.0..=240.0 => (0.0, x, c),
+        240.0..=300.0 => (x, 0.0, c),
+        _ => (c, 0.0, x),
+    };
 
-fn generate_chunk_color(x: i32, y: i32, z: i32) -> Color {
+    (r + m, g + m, b + m)
+}
+
+fn generate_chunk_color_checkered(x: i32, y: i32, z: i32) -> Color {
     // Determine if the chunk should be black based on the sum of its coordinates
     let is_black = (x + y + z) % 2 == 0;
 
@@ -104,6 +88,7 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut model: ResMut<ModelResource>,
+    model_settings: Res<ModelSettings>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
@@ -113,7 +98,8 @@ fn setup(
         directional_light: DirectionalLight {
             color: Color::srgb(0.98, 0.95, 0.82),
             shadows_enabled: true,
-            illuminance: 3_000., //light_consts::lux::OVERCAST_DAY,
+            // illuminance: 3_000., //light_consts::lux::OVERCAST_DAY,
+            illuminance: 6_000., //light_consts::lux::OVERCAST_DAY,
             // illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
             ..default()
         },
@@ -144,23 +130,14 @@ fn setup(
             brightness: 1000.0,
         })
         .insert(VolumetricFogSettings {
-            // This value is explicitly set to 0 since we have no environment map light
-            ambient_intensity: 0.5,
+            ambient_intensity: 0.1,
             ..default()
         })
-        // .insert(ScreenSpaceAmbientOcclusionBundle::default())
-        // .insert(ScreenSpaceAmbientOcclusionSettings {
-        //     quality_level: ScreenSpaceAmbientOcclusionQualityLevel::Ultra,
-        // })
-        // .insert(TemporalAntiAliasBundle::default())
-        ;
+        .insert(ScreenSpaceAmbientOcclusionBundle::default())
+        .insert(ScreenSpaceAmbientOcclusionSettings {
+            quality_level: ScreenSpaceAmbientOcclusionQualityLevel::Ultra,
+        });
 
-    // commands.spawn(EnvironmentMapLight {
-    //     diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
-    //     specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-    //     intensity: 2_000.0,
-    // });
-    //
     commands.spawn(
         TextBundle::from_section("Press space to toggle wireframes", TextStyle::default())
             .with_style(Style {
@@ -173,7 +150,6 @@ fn setup(
 
     let ground_material = materials.add(StandardMaterial {
         base_color: Color::from(GRAY),
-        // base_color: Color::WHITE,
         perceptual_roughness: 0.7,
         reflectance: 0.4,
         ..default()
@@ -200,8 +176,14 @@ fn setup(
 
         let chunk_position = model.chunks[i].get_position();
 
+        let base_color = if model_settings.material_type == MaterialType::Checkered {
+            generate_chunk_color_checkered(chunk_position.x, chunk_position.y, chunk_position.z)
+        } else {
+            generate_chunk_color_gradient(i, model.chunks.len())
+        };
+
         let mesh_material = materials.add(StandardMaterial {
-            base_color: generate_chunk_color(chunk_position.x, chunk_position.y, chunk_position.z),
+            base_color,
             perceptual_roughness: 1.0,
             reflectance: 0.0,
             ..default()
@@ -211,7 +193,6 @@ fn setup(
             .spawn(PbrBundle {
                 mesh,
                 material: mesh_material.clone(),
-                // transform: Transform::from_xyz(0.0, 0.0, -5.0),
                 transform: Transform::from_translation(chunk_position.as_vec3()),
                 ..default()
             })
@@ -237,14 +218,6 @@ fn setup(
         material: ground_material,
         ..default()
     });
-}
-
-fn rotate(mut query: Query<&mut Transform, With<Camera>>, _time: Res<Time>) {
-    // let cam = query.iter_mut().next().unwrap();
-    // println!("Cam: {:?}", cam.translation);
-    // for mut transform in &mut query {
-    //     transform.rotate_y(time.delta_seconds() / 2.);
-    // }
 }
 
 fn toggle_wireframe(
@@ -279,12 +252,10 @@ fn main() {
                 ..default()
             }),
             EguiPlugin,
-            // TemporalAntiAliasPlugin,
             GamePlugin,
             PanOrbitCameraPlugin,
             WireframePlugin,
             FrameTimeDiagnosticsPlugin,
-            // LogDiagnosticsPlugin::default(),
             ScreenDiagnosticsPlugin::default(),
             ScreenFrameDiagnosticsPlugin,
             ScreenEntityDiagnosticsPlugin,
@@ -296,6 +267,9 @@ fn main() {
             alpha: 1.0,
         })))
         .insert_resource(ModelResource(model))
+        .insert_resource(ModelSettings {
+            material_type: MaterialType::Checkered,
+        })
         .insert_resource(Msaa::Off)
         .run();
 
