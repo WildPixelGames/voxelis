fa4df261874d5d8e35d57e76cd31b5d3a634ef7c
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2024-10-06 21:28:52 +0200

    Remove old commented out code

--- voxelis/src/chunk.rs
+++ voxelis/src/chunk.rs
@@ -100,50 +100,6 @@ impl Chunk {
             }
         }
 
-        // self.data.set_value(0, 0, 0, 0, 1);
-        // self.data.set_value(0, 1, 0, 0, 1);
-        // self.data.set_value(0, 0, 0, 1, 1);
-        // self.data.set_value(0, 1, 0, 1, 1);
-        // self.data.set_value(0, 0, 1, 0, 1);
-        // self.data.set_value(0, 1, 1, 0, 1);
-        // self.data.set_value(0, 0, 1, 1, 1);
-        // self.data.set_value(0, 1, 1, 1, 1);
-
-        // self.data.set_value(0, 2, 0, 0, 1);
-        // self.data.set_value(0, 3, 0, 0, 1);
-        // self.data.set_value(0, 2, 0, 1, 1);
-        // self.data.set_value(0, 3, 0, 1, 1);
-        // self.data.set_value(0, 2, 1, 0, 1);
-        // self.data.set_value(0, 3, 1, 0, 1);
-        // self.data.set_value(0, 2, 1, 1, 1);
-
-        // self.data.set_value(0, 0, 0, 2, 1);
-        // self.data.set_value(0, 1, 0, 2, 1);
-        // self.data.set_value(0, 0, 0, 3, 1);
-        // self.data.set_value(0, 1, 0, 3, 1);
-        // self.data.set_value(0, 0, 1, 2, 1);
-        // self.data.set_value(0, 1, 1, 2, 1);
-
-        // self.data.set_value(0, 2, 0, 2, 1);
-        // self.data.set_value(0, 3, 0, 2, 1);
-        // self.data.set_value(0, 2, 0, 3, 1);
-        // self.data.set_value(0, 3, 0, 3, 1);
-        // self.data.set_value(0, 2, 1, 2, 1);
-
-        // self.data.set_value(0, 0, 2, 0, 1);
-        // self.data.set_value(0, 1, 2, 0, 1);
-        // self.data.set_value(0, 0, 2, 1, 1);
-        // self.data.set_value(0, 1, 2, 1, 1);
-
-        // self.data.set_value(0, 2, 2, 0, 1);
-        // self.data.set_value(0, 3, 2, 0, 1);
-        // self.data.set_value(0, 2, 2, 1, 1);
-
-        // self.data.set_value(0, 0, 2, 2, 1);
-        // self.data.set_value(0, 1, 2, 2, 1);
-
-        // self.data.set_value(0, 2, 2, 2, 1);
-
         self.data.update_lods();
     }
 
