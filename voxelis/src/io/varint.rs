7485c7dce3676067fc72a5f4b862246bba80543f
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2025-04-05 20:27:11 +0200

    refactor: improve varint

--- voxelis/src/io/varint.rs
+++ voxelis/src/io/varint.rs
@@ -1,3 +1,20 @@
+use std::io::BufReader;
+
+use byteorder::ReadBytesExt;
+
+pub fn encode_varint_u32(mut value: u32) -> Vec<u8> {
+    let mut bytes = Vec::with_capacity(5);
+
+    while value >= 0x80 {
+        bytes.push((value as u8 & 0x7F) | 0x80);
+        value >>= 7;
+    }
+
+    bytes.push(value as u8);
+
+    bytes
+}
+
 pub fn encode_varint(mut value: usize) -> Vec<u8> {
     let mut bytes = Vec::with_capacity(8);
 
@@ -11,6 +28,52 @@ pub fn encode_varint(mut value: usize) -> Vec<u8> {
     bytes.push(value as u8);
 
     bytes
+}
+
+pub fn decode_varint_u32(iter: &mut std::slice::Iter<u8>) -> Option<u32> {
+    let mut result = 0u32;
+    let mut shift = 0;
+
+    loop {
+        let byte = *iter.next()?;
+
+        result |= ((byte & 0x7F) as u32) << shift;
+
+        if byte & 0x80 == 0 {
+            break;
+        }
+
+        shift += 7;
+
+        if shift >= 32 {
+            return None;
+        }
+    }
+
+    Some(result)
+}
+
+pub fn decode_varint_u32_from_reader(reader: &mut BufReader<&[u8]>) -> Option<u32> {
+    let mut result = 0u32;
+    let mut shift = 0;
+
+    loop {
+        let byte = reader.read_u8().ok()?;
+
+        result |= ((byte & 0x7F) as u32) << shift;
+
+        if byte & 0x80 == 0 {
+            break;
+        }
+
+        shift += 7;
+
+        if shift >= 32 {
+            return None;
+        }
+    }
+
+    Some(result)
 }
 
 pub fn decode_varint(iter: &mut std::slice::Iter<u8>) -> Option<usize> {
