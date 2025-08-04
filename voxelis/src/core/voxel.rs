96370fc3d9c9378633cb277c354c8413e60b3157
Artur Wyszyński <artur.wyszynski@wildpixelgames.com>
2025-08-04 02:37:33 +0200

    feat: add ByteConversion trait to VoxelTrait for binary serialization
    - Add ByteConversion trait with big/little endian byte conversion methods
    - Include read/write methods for IO operations
    - Extend VoxelTrait bounds with Eq, PartialOrd, Ord, and ByteConversion
    - Implement ByteConversion for numeric types via macro under numeric_voxel_impls feature

--- voxelis/src/core/voxel.rs
+++ voxelis/src/core/voxel.rs
@@ -3,7 +3,38 @@ use std::hash::Hash;
 
 use crate::interner::MAX_CHILDREN;
 
-pub trait VoxelTrait: Clone + Copy + PartialEq + Default + Hash + Display + Debug {
+pub trait ByteConversion: Sized {
+    type ByteArray: AsRef<[u8]> + AsMut<[u8]> + Default;
+
+    fn to_be_bytes(&self) -> Self::ByteArray;
+    fn to_le_bytes(&self) -> Self::ByteArray;
+    fn from_be_bytes(bytes: Self::ByteArray) -> Self;
+    fn from_le_bytes(bytes: Self::ByteArray) -> Self;
+
+    fn read_from_be<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let mut bytes = Self::ByteArray::default();
+        reader.read_exact(bytes.as_mut())?;
+        Ok(Self::from_be_bytes(bytes))
+    }
+
+    fn read_from_le<R: std::io::Read>(reader: &mut R) -> std::io::Result<Self> {
+        let mut bytes = Self::ByteArray::default();
+        reader.read_exact(bytes.as_mut())?;
+        Ok(Self::from_le_bytes(bytes))
+    }
+
+    fn write_as_be<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(self.to_be_bytes().as_ref())
+    }
+
+    fn write_as_le<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        writer.write_all(self.to_le_bytes().as_ref())
+    }
+}
+
+pub trait VoxelTrait:
+    Default + Copy + Clone + Hash + PartialEq + Eq + PartialOrd + Ord + Display + Debug + ByteConversion
+{
     #[inline(always)]
     fn average(children: &[Self]) -> Self {
         calc_average(children)
@@ -11,6 +42,39 @@ pub trait VoxelTrait: Clone + Copy + PartialEq + Default + Hash + Display + Debu
 
     fn material_id(&self) -> usize;
 }
+
+macro_rules! impl_byte_conversion {
+    ($($t:ty),+) => {
+        $(
+            #[cfg(feature = "numeric_voxel_impls")]
+            impl ByteConversion for $t {
+                type ByteArray = [u8; std::mem::size_of::<Self>()];
+
+                #[inline(always)]
+                fn to_be_bytes(&self) -> Self::ByteArray {
+                    <$t>::to_be_bytes(*self)
+                }
+
+                #[inline(always)]
+                fn to_le_bytes(&self) -> Self::ByteArray {
+                    <$t>::to_le_bytes(*self)
+                }
+
+                #[inline(always)]
+                fn from_be_bytes(bytes: Self::ByteArray) -> Self {
+                    <$t>::from_be_bytes(bytes)
+                }
+
+                #[inline(always)]
+                fn from_le_bytes(bytes: Self::ByteArray) -> Self {
+                    <$t>::from_le_bytes(bytes)
+                }
+            }
+        )+
+    };
+}
+
+impl_byte_conversion!(u8, i8, u16, i16, u32, i32, u64, i64);
 
 macro_rules! impl_voxel_trait_for_numerics {
     ($($t:ty),+) => {
